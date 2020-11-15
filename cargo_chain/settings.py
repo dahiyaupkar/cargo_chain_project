24b43d671a2b77b8df87cf810f79e7e98534235f
Upkar Dahiya <dahiyaupkar@gmail.com>
2020-11-14 21:31:58 -0800

    added email address

--- cargo_chain/settings.py
+++ cargo_chain/settings.py
@@ -128,8 +128,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Email Settings
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'cargochainkamloops@gmail.com'
+EMAIL_HOST_PASSWORD = '2dMpNyEq2akLT5R'
 EMAIL_USE_TLS = True
 # EMAIL_USE_SSL = False
 
